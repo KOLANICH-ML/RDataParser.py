d3b8d95c1ea2d83267eefc2f8518e9c1bc91ccdc
KOLANICH <kolan_n@mail.ru>
2023-10-12 03:03:35 +0300

    We had to move to Codeberg

--- RDataParser/__init__.py
+++ RDataParser/__init__.py
@@ -1,3 +1,7 @@
+from warnings import warn
+
+warn("We have moved from M$ GitHub to https://codeberg.org/KOLANICH-ML/RDataParser.py , read why on https://codeberg.org/KOLANICH/Fuck-GuanTEEnomo .")
+
 from .r_data import *
 
 try:
